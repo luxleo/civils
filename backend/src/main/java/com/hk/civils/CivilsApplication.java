614008625ead842bcb5ecc4f4ffff8e956dea81b
lux00leo <lux00leo@gmail.com>
2025-05-08 15:41:39 +0900

    initial setting

---
+++ backend/src/main/java/com/hk/civils/CivilsApplication.java
@@ -0,0 +1,13 @@
+package com.hk.civils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CivilsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hk/civils/CivilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hk.civils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
